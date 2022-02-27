e50b279e5bd7bf06f05c53acb2b8fab1240b7563
dzw <dzw071993@163.com>
2022-02-27 17:19:17 +0800

    update jsons

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <memory>
+#include <string>
 
 using namespace std;
+class Student
+{
+    public:
+    string name;
+    int score;
+    Student(string _name, int _score):name(_name), score(_score)
+    {
+
+    };
+    ~Student()
+    {
+
+    };
+};
+
+
 int main(int argc, char**  argv)
 {
     cout << "Hello World!" << endl;
     vector<int> arr;
-    arr.resize(5);
-    arr.at(0) = 4;
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr.at(i) << endl;
-    }
+    std::shared_ptr<int> fix1(new int);
+    // shared_ptr是个类， 所以不能写成std::shared_ptr<int> p = new int(1); 
+    std::shared_ptr<Student> li(new Student("LiHua", 96));
+    std::shared_ptr<Student> wang = std::make_shared<Student>("Wang", 98);
+    std::cout << li->name << " " << li->score << std::endl;
+    std::cout << wang->name << " " << wang->score << std::endl;
     return 0;
 }
