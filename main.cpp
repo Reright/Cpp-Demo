4bf42305bb23bb8477e505550396fdf5be1dacd2
dzw <dzw071993@163.com>
2022-03-13 19:10:48 +0800

    重载

--- main.cpp
+++ main.cpp
@@ -5,8 +5,23 @@
 
 using namespace std;
 
-int main(int argc, char**  argv)
+class A {
+public:
+    void fun(int a) { cout << "fun with int" << endl; }
+    void fun(double a) { cout << "fun with double" << endl; }
+    // bool fun(int a) { cout << "fun with int return bool" << endl; }  // 无法重载仅按返回值类型区分的函数
+    void fun(int a, float b) { cout << "fun with int and float" << endl; }
+    void fun(float a, int b) { cout << "fun with float and int" << endl; }
+    virtual void fun(int a, float b)  const { cout << "fun with int and float const" << endl; }  //编译通过，如何调用？
+};
+
+void main(int argc, char**  argv)
 {
     cout << "Hello World!" << endl << endl << endl;
-    return 0;
+    A* a = new A;
+    a->fun(1);
+    a->fun(1.009);
+    a->fun(1, 1.09);
+    a->fun(1.09, 1);
+    return;
 }
