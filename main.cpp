a94293c46455e48b52bb743fc107e5a5709c996d
dzw <dzw071993@163.com>
2022-03-24 20:27:59 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,30 @@
 
 using namespace std;
 
+class A {
+public:
+    A(int a):ca(a)//sca(a)//sa(a)
+    {
+        na = a;
+        // ca = a;
+        // sa = a;
+        // sca = a;
+    }
+
+private:
+    int na = 3;  // 不能在类外部初始化；可以在初始化列表中初始化；可以在构造函数中初始化；接受声明时的默认值
+    const int ca = 3;  // 不能在类外部初始化；可以在初始化列表中初始化；不能在构造函数中初始化；接受声明时的默认值
+    static int sa; //= 3; // 可以在类外部初始化；不能在初始化列表中初始化；不能在构造函数中初始化；不接受声明时的默认值
+    static const int sca = 3; //可以在类外部初始化(在外部初始化时必须加上const)；不能在初始化列表中初始化；不能在构造函数中初始化，接受声明时的默认值
+};
+
+int A::sa = 4;
+// const int A::sca = 5;
+
 int main(int argc, char**  argv)
 {
     cout << "Hello World!" << endl << endl << endl;
+    A* pa1 = new A(2);
+
     return 0;
 }
