a0905da9648543b240daa8a2af5d82521ef82f57
dzw <dzw071993@163.com>
2022-03-28 11:05:54 +0800

    增加头文件

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <memory>
-#include <string>
+#include "main.h"
 
 using namespace std;
 
---
+++ main.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+
+using namespace std;
