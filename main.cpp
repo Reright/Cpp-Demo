4751ec1c66941987b0a181e73e634b5d268a9f87
dzw <dzw071993@163.com>
2022-03-28 17:07:15 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
+A g_a(0);
+
 int main(int argc, char**  argv)
 {
     cout << "Hello World!" << endl << endl << endl;
+    GetA();
     return 0;
 }
--- main.h
+++ main.h
@@ -4,3 +4,28 @@
 #include <string>
 
 using namespace std;
+
+class A
+{
+public:
+    A()
+    {
+        cout << "调用全局变量 A 构造函数" << endl;
+    }
+    A(int a):m_a(a)
+    {
+        cout << "调用 A 构造函数, 静态属性: " << m_a << endl;
+    }
+    ~A()
+    {
+        cout << "调用 A 析构函数, 静态属性: " << m_a << endl;
+    }
+private:
+    int m_a;
+};
+
+void GetA()
+{
+    cout << "进入 GetA 函数执行" << endl;
+    static A a(1);
+}
