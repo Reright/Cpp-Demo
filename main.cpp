127cd77901c6d0586afac09160c8102827ff7fb7
dzw <dzw071993@163.com>
2022-04-02 17:34:56 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,68 @@
 #include "main.h"
-
+ 
 using namespace std;
+ 
+class BoxCaller
+{
+public:
+	//设置长宽高
+    void setLength( double len ){
+        box.length = len;  // 可以访问Box的private变量
+    }
+    void setBreadth( double bre ){
+        box.breadth = bre;
+    }
+    void setHeight( double hei ){
+        box.height = hei;
+    }
 
+    void Show()
+    {
+        cout << "BoxCaller has box: " << box << endl;
+    }
+ 
+	//获取长宽高
+	double getLength(){
+        return box.length;
+    }
+	double getBreadth(){
+        return box.breadth;
+    }
+	double getHeight(){
+        return box.height;
+    }
+ 
+	//获取体积
+	double getVolume(){
+		return box.getVolume();
+	}
+	//获取面积
+	double getArea(){
+		return box.getArea();
+	}
+ 
+private:
+      Box box;
+};
+ 
 int main(int argc, char**  argv)
 {
-    cout << "Hello World!" << endl << endl << endl;
-    return 0;
+	BoxCaller caller;
+	caller.setLength(5);
+    cout << "length : " << caller.getLength() << endl;
+ 
+    caller.setBreadth(6);
+    cout << "breadth : " <<  caller.getBreadth() << endl;
+ 
+	caller.setHeight(7);
+    cout << "height : " << caller.getHeight() << endl;
+ 
+	double volume = caller.getVolume();
+	cout << "volume : " << volume << endl;
+ 
+	double area = caller.getArea();
+	cout << "area : " << area << endl;
+
+    caller.Show();
+	return 0;
 }
--- main.h
+++ main.h
@@ -4,3 +4,35 @@
 #include <string>
 
 using namespace std;
+
+#include <iostream>
+using namespace std;
+ 
+class Box
+{
+public:
+	  friend class BoxCaller;
+      friend ostream& operator<<(ostream& os, Box& box)  // 可以作为教科书般的用法了
+      {
+        os << "length: "  << box.length << " breadth: " << box.breadth 
+           << " height: " << box.height << endl;
+        return os;
+      }
+private:
+      double getVolume(void);
+	  double getArea(void);
+   
+      double length;      // 长度
+      double breadth;     // 宽度
+      double height;      // 高度
+};
+
+double Box::getVolume(void)
+{
+    return length * breadth * height;
+}
+ 
+double Box::getArea(void)
+{
+    return length * breadth;
+}
