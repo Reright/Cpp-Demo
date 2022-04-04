f39cc4a23f40a45f85c9b8d8a75e5379c6cca457
dzw <dzw071993@163.com>
2022-04-04 13:19:34 +0800

    一个成功的TimingFifo，主体结构仅用map实现

--- main.cpp
+++ main.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
+
+void Test()
+{
+    map<int, std::string> list;
+    // list.insert(std::make_pair<1, "LiHua">); // 为什么不对--make_pair是函数，后面两个参数用小括号包含
+    list.insert(std::make_pair(1, "LiHua"));
+    list.insert(std::make_pair(4, "LiHua"));
+    list.insert(std::make_pair(7, "LiHua"));
+    list.insert(std::make_pair(2, "LiHua"));
+    list.insert(std::make_pair(8, "LiHua"));
+    list.insert(std::make_pair(10, "LiHua"));
+    for (auto it = list.cbegin(); it != list.cend(); it++) {
+        cout << it->first << "---" << it->second << endl;
+    }
+}
+
 int main(int argc, char**  argv)
 {
     cout << "Hello World!" << endl << endl << endl;
+    Fifo fifo(16, 2, "issueq");  // fifo深度16，默认时延2cycle
+    fifo.write(10, new Inst(Add));  // 在第10cycle写入一个Add指令
+    fifo.write(11, new Inst(Sub)); 
+    
+    
+    cout << fifo << endl;
+
+    cout << "At cycle 12: " << fifo.read(12)->GetType() << endl;
+    fifo.write(12, new Inst(Load));  
+    cout << "At cycle 13: " << fifo.read(13)->GetType() << endl;
+    fifo.write(13, new Inst(Store)); 
+    cout << "At cycle 14: " << fifo.read(14)->GetType() << endl;
+    for(int i = 15; i < 35; i++) {
+        fifo.write(i, new Inst(Sub));
+    }
+    // cout << fifo << endl;
     return 0;
 }
--- main.h
+++ main.h
@@ -2,5 +2,125 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <queue>
+#include <map>
 
 using namespace std;
+
+enum InstType {
+    Add = 0,
+    Sub,
+    Div,
+    Load,
+    Store,
+    Unimplemented
+};
+
+class Inst{
+public:
+
+    friend ostream& operator<<(ostream& os, Inst& inst) {
+        os << inst.GetType();
+        return os;
+    }
+
+    Inst(InstType type): m_type(type)
+    {
+
+    }
+    void SetType(InstType type)
+    {
+        m_type = type;
+    }
+    
+    InstType GetType()
+    {
+        return m_type;
+    }
+private:
+    InstType m_type = Unimplemented;
+};
+
+
+/*
+** Fifo通过std::map保证存入的元素按照first严格保序
+*/
+class Fifo{
+public:
+
+    friend ostream& operator<<(ostream& os, Fifo& f) {
+        if(!f.q.empty()){
+            for(auto it = f.q.cbegin(); it != f.q.cend(); it++) {
+                os << "Timing: " << it->first << " , InstType: " << it->second->GetType() << endl;
+            }
+        } else {
+            cout << "No Ready Element in Fifo!" << endl;
+        }
+        return os;
+    }
+
+    Fifo(int depth, int lat, const char* name): m_maxSize(depth), m_lat(lat), m_name(name)
+    {
+        // q.resize(depth);  // resize会限制最大深度吗？超过深度会报错吗？std::queue没有resize方法？
+        // 哪些数据结构有resize方法？ 为什么其他的没有？
+    }
+    virtual ~Fifo()
+    {
+
+    }
+
+    bool write(int now, Inst* inst)
+    {
+        if(q.size() < m_maxSize) {  // q.size()返回的是map的表项个数吗？
+            q.insert(std::make_pair(now + m_lat, inst));
+            return true;
+        } else {
+            cout << "Fifo [" << m_name << "] is Full!" << endl;
+            return false;
+        }
+    }
+
+    Inst* read(int now)  // 要求读出前面的指令，并从Fifo中删除
+    {
+        if(!q.empty()){
+            for(auto it = q.cbegin(); it != q.cend(); it++) {
+                if(it->first <= now) {
+                    Inst* tmp = it->second;
+                    q.erase(it);
+                    return tmp;
+                }
+            }
+        } else {
+            cout << "No Ready Element in Fifo!" << endl;
+            return nullptr;
+        }
+        return nullptr;
+    }
+
+    Inst* peek(int now)  // 要求读出前面的指令，并从Fifo中删除
+    {
+        if(!q.empty()){
+            for(auto it = q.cbegin(); it != q.cend(); it++) {
+                if(it->first <= now) {
+                    Inst* tmp = it->second;
+                    return tmp;
+                }
+            }
+        } else {
+            cout << "No Ready Element in Fifo!" << endl;
+            return nullptr;
+        }
+        return nullptr;
+    }
+
+private:
+    int m_maxSize;
+    int m_lat;  // Fifo的默认时延
+    const char* m_name;
+    map<int, Inst*> q;  // map的正确用法？ map会默认按照first排序？排序规则是什么？
+    // 默认按照第一个参数升序排列
+    // std::queue<entry*> q;  // queue不是模板类？
+    // vector<Entry*> q;
+    // vector pop_front之后后面的元素会向前前进一个索引吗？
+    typedef map<int, Inst*>::iterator QItem;    // iterator怎么用？不是作用域解析符吗？
+};
